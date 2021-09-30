af0a27d55a2a10cb92e1526baf309b201a3bab3c
Sergery Rutkousky <dr.bibelman@gmail.com>
2021-10-01 01:22:29 +0300

    сохраняем прогресс

---
+++ dist/scripts/script.js
@@ -0,0 +1,5 @@
+function perfectpixel() {
+  document.getElementById('perfectpixel').classList.toggle('hide');
+}
+
+document.getElementById("perfectpixel-btn").addEventListener("click", perfectpixel);
