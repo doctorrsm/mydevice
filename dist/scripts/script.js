cf221aae7806ba5b43ef5f3fb32ef3ef764e2128
Sergery Rutkousky <dr.bibelman@gmail.com>
2021-10-02 13:43:36 +0300

    save

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,5 +1,24 @@
 function perfectpixel() {
-  document.getElementById('perfectpixel').classList.toggle('hide');
+  var el =  document.getElementById('perfectpixel');
+  el.classList.toggle('show');
+  el.classList.toggle('bg-index');
+
+}
+function monopod() {
+  var el =  document.getElementById('perfectpixel');
+  el.classList.toggle('show');
+  el.classList.toggle('bg-monopod');
+
+}
+function grid() {
+  var el =  document.getElementById('perfectpixel');
+  el.classList.toggle('show');
+  el.classList.toggle('bg-grid');
+
 }
 
 document.getElementById("btn--index").addEventListener("click", perfectpixel);
+
+document.getElementById("btn--monopod").addEventListener("click", monopod);
+
+document.getElementById("btn--grid").addEventListener("click", grid);
